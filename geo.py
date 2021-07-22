02e0015f003ba833f8e3a7aa00b17fadec2dc723
u7biliar <boris.iliarski@zeiss.com>
2021-07-22 15:56:54 -0500

    Added numpy and trace reading

--- geo.py
+++ geo.py
@@ -2,6 +2,17 @@ from segpy.reader import create_reader
 from segpy.writer import write_segy
 
 
+import numpy as np
+
+
+"""
+Docs:
+    Segpy https://segpy.readthedocs.io/en/latest/
+    
+    NumPy https://docs.scipy.org/doc/numpy-1.13.0/reference/index.html
+"""
+
+
 if __name__ == '__main__':
     with open('data/CUTE.sgy', 'rb') as segy_in_file:
         # The seg_y_dataset is a lazy-reader, so keep the file open throughout.
@@ -10,3 +21,12 @@ if __name__ == '__main__':
         # # Write the seg_y_dataset out to another file, in big-endian format
         # with open('seismic_big.sgy', 'wb') as segy_out_file:
         #     write_segy(segy_out_file, seg_y_dataset, endian='>')  # Standard Rev 1 big-endian
+
+        data = []
+        for i in range(0, seg_y_dataset.num_traces()):
+            trace = seg_y_dataset.trace_samples(i)
+            data.append(trace)
+
+        data_matrix = np.array(data, np.int32)
+
+        print(data_matrix)
